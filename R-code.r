a30b15e95c1a4950e0d1ad4f8abb5a7f5e51e378
Jasper Wubs <e.r.j.wubs@gmail.com>
2018-05-02 11:44:39 +0200

    data load and PCA
    Added code to open a .csv datafile from github and run a PCA on the data. Note: the url to the .csv should be 'raw'  (see button in GitHub).

--- R-code.r
+++ R-code.r
@@ -1,7 +1,13 @@
-########## Some R code to try.
+########## .
+##########################################################
+# Some R code to try
+# data from: Trait data selected.csv
+#########################################################
+rm(list=ls())
 
-source( "https://github.com/JasperWubs/R-test/edit/master/R-code")
+d <- read.csv("https://raw.githubusercontent.com/JasperWubs/R-test/master/Trait%20data%20selected.csv")
+d2 <- na.omit(d)
+library(vegan)
 
-library(RCurl)#;install.packages("RCurl")
-d <- read.csv(text = getURL("http://github.com/JasperWubs/R-test/blob/master/Trait%20data%20selected.csv"))
-d <- read.csv("http://github.com/JasperWubs/R-test/blob/master/Trait%20data%20selected.csv")
+pca1 <- rda(d2[,5:31])
+plot(pca1)
