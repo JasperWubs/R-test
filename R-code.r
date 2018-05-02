3ce680fca9cac7828a172447f1fd0db62cc312ae
Jasper Wubs <e.r.j.wubs@gmail.com>
2018-05-02 11:30:47 +0200

    more code

--- R-code.r
+++ R-code.r
@@ -1,1 +1,7 @@
 ########## Some R code to try.
+
+source( "https://github.com/JasperWubs/R-test/edit/master/R-code")
+
+library(RCurl)#;install.packages("RCurl")
+d <- read.csv(text = getURL("http://github.com/JasperWubs/R-test/blob/master/Trait%20data%20selected.csv"))
+d <- read.csv("http://github.com/JasperWubs/R-test/blob/master/Trait%20data%20selected.csv")
